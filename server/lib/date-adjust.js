7ba46310b094a79311fc984d66d1351d4273aa83
Yen Nguyen <yen.hnguyen17@gmail.com>
2021-12-10 06:15:57 +0000

    Initial commit

--- server/lib/date-adjust.js
+++ server/lib/date-adjust.js
@@ -10,10 +10,11 @@ let tweetsJSON = require('../data-files/initial-tweets.json');
 module.exports = () => {
   // One day in milliseconds is 86400000ms or...
   // oneDayMs = 1000 milliseconds * 60 seconds * 60 minutes * 24 hours.
-  const oneDayMs = 1000 * 60 * 60 * 24
+  const oneDayMs = 1000 * 60 * 60 * 24;
   // Subtract one day in milliseconds (oneDayMs) times the tweets length minus the current index.
   // This keeps the newest tweets at the bottom, and allows for further tweets to be added.
   tweetsJSON = tweetsJSON.map((tweet, index) => {
+    // eslint-disable-next-line camelcase
     tweet.created_at = Date.now() - (oneDayMs * (tweetsJSON.length - index));
     return tweet;
   });
