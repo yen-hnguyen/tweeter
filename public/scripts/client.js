4611f93d6455e7cbbe06f809b39132e75050bc5f
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-01-06 04:42:26 +0000

    Clean up code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -15,7 +15,6 @@ const escape = function(str) {
 $(document).ready(function() {
   $(".error").hide();
 
-
   const createTweetElement = function(tweet) {
     let tweetTime = timeago.format(escape(tweet.created_at));
     let $tweet = $(`
@@ -64,7 +63,7 @@ $(document).ready(function() {
 
   const formValidation = function() {
     const $tweetContent = $("#tweet-text").val();
-    
+
     $(".error").hide();
 
     if (!$tweetContent.length) {
@@ -90,29 +89,3 @@ $(document).ready(function() {
 
 
 });
-
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
   $("#tweet-text").on("input", function() {
     let counter = $(this).parent().find('output');
     $(counter).text(140 - $(this).val().length);
-    console.log($(counter).val());
     if ($(counter).val() < 0) {
       $(counter).css('color', 'red');
     } else {
