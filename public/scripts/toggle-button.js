9a79a7dded315f3b5b5fac3660c5d56c6e6ddbc6
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-01-11 03:28:02 +0000

    Hide the tweet-form initially

--- public/scripts/toggle-button.js
+++ public/scripts/toggle-button.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-undef */
 $(document).ready(function() {
+  $(".tweet-form").hide();
   $(".compose-button").on("click", function() {
-    $(".tweet-form").slideToggle();
+    $(".tweet-form").slideToggle("slow");
     $("#tweet-text").focus();
   });
 });
